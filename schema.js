17bf5d78cecbca05ef26f1a0f7cd6d774835196d
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-18 15:59:10 -0500

    Basic schema updates

---
+++ schema.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+exports.typeDefs = gql `
+   type Hotel {
+     id: ID!
+     hotel_name: String!
+     street: String!
+     city: String!
+     postal_code: String!
+     price: Float!
+     email: String!
+   }
+   type Query {
+     getHotel: [Hotel]
+     getHotelByID(id: ID!): Hotel
+     getHotelByCity(city: String!): [Hotel]
+   }
+   type Mutation {
+     addHotel(hotel_name: String!
+        street: String!
+        city: String!
+        postal_code: String!
+        price: Float!
+        email: String!): Hotel
+     updateHotel(id: String!,
+        hotel_name: String!
+        street: String!
+        city: String!
+        postal_code: String!
+        price: Float!
+        email: String!): Hotel
+     deleteHotel(id: ID!): Hotel
+   }
+`
