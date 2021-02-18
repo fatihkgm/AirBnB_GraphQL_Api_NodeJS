e38debf8d4d314b25c6ada70ba19f253b6bed761
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-18 16:09:23 -0500

    HotelSchema in model updates

---
+++ models/Hotel.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const HotelSchema = new mongoose.Schema({
+  hotel_name: {
+    type: String,
+    required: [true, 'Please enter hotel name'],
+    trim: true,
+    lowercase: true
+  },
+  street: {
+    type: String,
+    alias: 'street',
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  city: {
+    type: String,
+    alias: 'city',
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  postal_code: {
+    type: String,
+    alias: 'postal_code',
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  price: {
+    type: Number,
+    default: 0.0,
+    //min: [1000, 'Too less Salary'],
+    //max: 12000,
+    validate(value) {
+      if (value < 0.0){
+         throw new Error("Please enter acceptable price ");
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    //index: true, //Optional if unique is defined
+    unique: [true, "Duplicate Email Not allowed"],
+    trim: true,
+    uppercase: true,
+    //minlength:10,
+    //maxlength: 50,
+    //Custom validation
+    validate: function(value) {
+      var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+      return emailRegex.test(value);
+    }
+  },
+});
+
+const Hotel = mongoose.model("Hotel", HotelSchema);
+module.exports = Hotel;
