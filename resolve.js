73d9842e18466fb96c674c3a4d18479bf43d0a0e
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-18 15:52:27 -0500

    Exporting Hotels information updated

---
+++ resolve.js
@@ -0,0 +1,73 @@
+const Employee = require('./models/Hotel');
+
+exports.resolvers = {
+    Query: {
+        getHotel: async (parent, args) => {
+            return await Employee.find({});
+        },
+        getHotelByID: async (parent, args) => {
+            return await Employee.findById(args.id);
+        },
+        getHotelByCity: async (parent, args) => {
+            return await Employee.find({"city" : args.city});
+        }
+    },
+    Mutation: {
+        addHotel: async (parent, args) => {
+            console.log(args)
+            const emailExpression = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            const isValidEmail =  emailExpression.test(String(args.email).toLowerCase())
+            
+            if(!isValidEmail){
+                throw new Error("email not in proper format")
+            }
+
+            let newHotel = new Hotel({
+                hotel_name: args.hotel_name,
+                street: args.street,
+                city:args.city,
+                postal_code:args.postal_code,
+                price:args.price,
+                email: args.email,
+                
+            });
+        return await newHotel.save();
+      },
+      updateHotel: async (parent, args) => {
+            console.log(args)
+            if (!args.id){
+                return;
+            }
+            return await Hotel.findOneAndUpdate(
+            {
+                _id: args.id
+            },
+            {
+                $set: {
+                    hotel_name: args.hotel_name,
+                    street: args.street,
+                    city:args.city,
+                    postal_code:args.postal_code,
+                    price:args.price,
+                    email: args.email,
+                }
+            }, {new: true}, (err, hotel) => {
+                if (err) 
+                {
+                    console.log('Something went wrong when updating the hotel');
+                } else 
+                {
+                    return hotel
+                }
+            }
+        );
+      },
+      deleteHotel: async (parent, args) => {
+        console.log(args)
+        if (!args.id){
+            return JSON.stringify({status: false, "message" : "No ID found"});
+        }
+        return await Hotel.findByIdAndDelete(args.id)
+      },
+    }
+  }
