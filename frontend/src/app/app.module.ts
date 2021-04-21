e0dd3b089ff27fa31ac5cada0c35046b12fb1b48
Felix Gokmen <63836841+fatihkgm@users.noreply.github.com>
2021-04-21 01:04:53 -0400

    pages created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    LoginComponent,
+    LogoutComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
