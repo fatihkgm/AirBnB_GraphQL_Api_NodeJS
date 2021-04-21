608b2e0e7930b7ccb1e0fa28f370815dd40c19ec
Felix Gokmen <63836841+fatihkgm@users.noreply.github.com>
2021-04-21 01:58:02 -0400

    update on login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component'
+import { LogoutComponent } from './logout/logout.component'
+import { AboutComponent } from './about/about.component'
+import { ValidationguardGuard } from './validationguard.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'logout', component: LogoutComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
