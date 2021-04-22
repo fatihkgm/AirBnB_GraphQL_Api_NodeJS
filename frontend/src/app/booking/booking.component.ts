c1f10828ddf97f0fc9e2cbfef450233a5c56a936
Felix Gokmen <63836841+fatihkgm@users.noreply.github.com>
2021-04-22 18:38:27 -0400

    hotel fetch data implemented

--- frontend/src/app/booking/booking.component.ts
+++ frontend/src/app/booking/booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
 
 @Component({
   selector: 'app-booking',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
-  constructor() { }
+  hotels: any[] = [];
+  loading = true;
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
+    this.apollo.query<any>({
+      query:gql `{getHotel{hotel_name street city postal_code}}`
+    })
+    .subscribe(
+      ({data,loading}) =>{
+        this.hotels = data && data.hotels;
+        this.loading = loading;
+      }
+    );
   }
-
 }
+
